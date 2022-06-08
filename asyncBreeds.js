be834efc5bffe52de1a3ffaae7026c75d4f6347c
Derine Banis <d.banis.13@gmail.com>
2022-06-07 17:09:08 -0700

    data on the 2 types of cats

---
+++ asyncBreeds.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed) {
+  let someotherdata = fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    if (!error) {
+      console.log('breedDetailsFromFile: Calling readFile...');
+      console.log(`${breed} Cat Data`);
+      console.log(`Description: ${data} \n`);
+    }
+  });
+};
+breedDetailsFromFile('Bombay');
+breedDetailsFromFile('Balinese')
